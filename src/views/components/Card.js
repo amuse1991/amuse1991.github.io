26c805a2a52c02e471e5603a0c01db64b2226e08
amuse1991 <amuse1991@gmail.com>
2020-08-11 15:04:48 +0900

    Card 컴포넌트 추가

---
+++ src/views/components/Card.js
@@ -0,0 +1,29 @@
+export default class Card {
+  $target = null;
+  $card = null;
+  data = null;
+
+  constructor({ $target, data }) {
+    this.$target = $target;
+    this.$card = document.createElement("article");
+    this.$card.setAttribute("class", "card");
+    this.$target.appendChild(this.$card);
+    this.data = data;
+    console.log(data);
+
+    this.render();
+  }
+
+  render() {
+    const { title, tags, titleDesc, thumbFileName } = this.data;
+    this.$card.innerHTML = `
+      <h2 class="card__title">${title}</h2>
+      <img class="card__thumb" src="static/${thumbFileName}" alt="${title} thumbnail">
+      <ul class="card__tag-group"> 
+        ${tags.map(tag => `<li class="card__tag">${tag}</li>`).join("")}
+      </ul>
+      <p class="">${titleDesc}</p>
+      <button type="button" class="card__button">learn more</button>
+    `;
+  }
+}
